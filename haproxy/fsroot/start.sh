6d846de361ef490655ccf5f7600f4c29d4ee9396
Bertrand Delacretaz <bdelacretaz@apache.org>
2014-08-20 08:18:48 +0200

    reorganize and simplify

--- haproxy/fsroot/start.sh
+++ haproxy/fsroot/start.sh
@@ -1,10 +1,11 @@
 service haproxy start
 
 ETCD=${ETCD_PORT_4001_TCP_ADDR}:${ETCD_PORT_4001_TCP_PORT}
-if [ -z "$ETCD" ]
+if [ "$ETCD" = “:” ]
 then
   echo "ERROR: missing ETCD environment variables"
   exit 1
 fi
+
 echo Using etcd server $ETCD
-confd -interval 1 -node $ETCD
+confd -node $ETCD -interval $CONFD_INTERVAL
